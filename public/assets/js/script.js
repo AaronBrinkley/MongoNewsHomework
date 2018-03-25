fcf7edae3ce156309a1bcc22b785e87db3e7dbdb
Aaron <aaronl.brinkley@gmail.com>
2018-03-25 19:32:21 -0400

    Button Functionality Troubleshoot

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -73,43 +73,80 @@ $(document).ready(function() {
 
 
 
-
+            console.log("Hello")
              
-    $(".Note-Form").on("submit", function(event) {
+    $(".Note-Form").on("click", "#Note-Button", function(event) {
      event.preventDefault();
+     $("#notes").empty();
     console.log("button works")
+    // $('#myModal').modal('toggle')
     
    
-    // var Savedheadline_id = $(this).children(".Savedheadline_id").val();
-  
-  
-    // $.ajax({
-    //   method: "GET",
-    //   url: "/headlines/" + Savedheadline_id
-    // })
-     
-    //   .then(function(data) {
-    //     console.log(data);
-      
-    //     $("#notes").append("<h2>" + data.title + "</h2>");
-      
-    //     $("#notes").append("<input id='titleinput' name='title' >");
-     
-    //     $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      
-    //     $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+    var Savedheadline_id = $(this).children(".Savedheadlinenote_id").val();
 
-    //     ("#myModal").style.display = "block";
+    console.log(Savedheadline_id)
+  
+  
+    $.ajax({
+      method: "GET",
+      url: "/headlines/" + Savedheadline_id
+    })
+     
+      .then(function(data) {
+          
+        console.log(data);
+      
+        $("#notes").append("<h2>" + data.title + "</h2>");
+      
+        $("#notes").append("<input id='titleinput' name='title' >");
+     
+        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      
+        $("#notes").append("<button class='btn btn-default' data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+        
   
      
-    //     if (data.note) {
+        if (data.note) {
          
-    //       $("#titleinput").val(data.note.title);
+          $("#titleinput").val(data.note.title);
          
-    //       $("#bodyinput").val(data.note.body);
-    //     }
-    //   });
+          $("#bodyinput").val(data.note.body);
+        }
+      });
   });
+
+
+
+  
+$(document).on("click", "#savenote", function() {
+    
+    var noteId = $(this).attr("data-id");
+  
+    
+    $.ajax({
+      method: "POST",
+      url: "/headlines/" + noteId,
+      data: {
+       
+        title: $("#titleinput").val(),
+        
+        body: $("#bodyinput").val()
+      }
+    })
+      
+      .then(function(data) {
+        
+        console.log(data);
+       
+        $("#notes").empty();
+      });
+  
+    
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+  });
+  
 
 
 
